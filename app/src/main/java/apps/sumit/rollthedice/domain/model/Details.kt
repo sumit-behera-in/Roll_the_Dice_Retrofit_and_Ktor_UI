f33db59b8f496513df2c796d3196d1d8fc8ee96d
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-04-04 16:43:09 +0530

    data models done

---
+++ app/src/main/java/apps/sumit/rollthedice/domain/model/Details.kt
@@ -0,0 +1,9 @@
+package apps.sumit.rollthedice.domain.model
+
+data class Details(
+    val email: String,
+    val github: String,
+    val linkedIn: String,
+    val mobile: String,
+    val name: String,
+)
---
+++ app/src/main/java/apps/sumit/rollthedice/domain/model/Dice.kt
@@ -0,0 +1,7 @@
+package apps.sumit.rollthedice.domain.model
+
+data class Dice(
+    val description: String,
+    val imageUrl: String,
+    val name: String,
+)
