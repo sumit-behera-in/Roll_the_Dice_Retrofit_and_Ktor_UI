7586a53e49f396db9dfdf6195ecfa1c44742d65f
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-04-04 20:33:59 +0530

    always add launch(viewmodelscope) after a usecase

--- app/src/main/java/apps/sumit/rollthedice/presentation/features/mainScreen/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/apps/sumit/rollthedice/presentation/features/mainScreen/viewmodel/MainScreenViewModel.kt
@@ -1,15 +1,20 @@
 package apps.sumit.rollthedice.presentation.features.mainScreen.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import apps.sumit.rollthedice.common.Resource
 import apps.sumit.rollthedice.domain.model.Details
 import apps.sumit.rollthedice.domain.model.Dice
 import apps.sumit.rollthedice.domain.use_case.GetDetailsUseCase
 import apps.sumit.rollthedice.domain.use_case.GetDiceUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@HiltViewModel
 class MainScreenViewModel @Inject constructor(
     private val getDetailsUseCase: GetDetailsUseCase,
     private val getDiceUseCase: GetDiceUseCase,
@@ -19,9 +24,11 @@ class MainScreenViewModel @Inject constructor(
 
     init {
         getDetails()
+        getDice()
     }
 
     private fun getDetails() {
+        Log.d("api", " getDetails view model")
         getDetailsUseCase().onEach { resource: Resource<Details> ->
             when (resource) {
                 is Resource.Error -> {
@@ -36,10 +43,11 @@ class MainScreenViewModel @Inject constructor(
                     _state.value = GetDiceState(details = resource.data)
                 }
             }
-        }
+        }.launchIn(viewModelScope)
     }
 
     fun getDice() {
+        Log.d("api", " getDice view model")
         getDiceUseCase().onEach { resource: Resource<Dice> ->
             when (resource) {
                 is Resource.Error -> {
@@ -54,6 +62,6 @@ class MainScreenViewModel @Inject constructor(
                     _state.value = GetDiceState(dice = resource.data)
                 }
             }
-        }
+        }.launchIn(viewModelScope)
     }
 }
